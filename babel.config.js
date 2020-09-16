6668a6c619506c679a67caddc5a479f6573db2dd
Dane Iracleous <daneiracleous@gmail.com>
2020-09-16 10:51:17 -0700

    babel fix

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,15 @@
 const devPresets = ['@vue/babel-preset-app'];
-const buildPresets = ['@babel/preset-env', '@babel/preset-typescript'];
+
+const buildPresets = [
+  [
+    "@babel/preset-env", {
+      "targets": {
+        "node": "current"
+      }
+    }
+  ],
+  '@babel/preset-typescript'
+];
 module.exports = {
   presets: (process.env.NODE_ENV === 'development' ? devPresets : buildPresets),
 };
